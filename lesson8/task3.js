8dd2649822b373e7cbba01c517e0afd0bfe1bda7
dashatolstaya <dasha.tolstaya18@gmail.com>
2024-09-17 16:26:25 +0300

    Add arrays tasks

---
+++ lesson8/task3.js
@@ -0,0 +1,5 @@
+const numbers = [10, 25, 37, 40, 60];
+
+const sum = numbers.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+
+console.log(sum);
---
+++ lesson8/task5.js
@@ -0,0 +1,6 @@
+const firstArray = [1, 2, 3];
+const secondArray = [4, 5, 6, 7, 8, 9, 10];
+
+const combinedArray = firstArray.concat(secondArray);
+
+console.log(combinedArray);
